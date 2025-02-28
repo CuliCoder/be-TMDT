0b6e58533ced8ed1f0e57c96bde98f5f5716e5b3
trungnc113 <nguyencongtrung13@gmail.com>
2025-02-28 19:10:42 +0700

    merge code

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,7 @@ import "dotenv/config";
 const connection = database.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-<<<<<<< HEAD
-  password: process.env.DB_PASSWORD,
-=======
   password: process.env.DB_PASSWORD || "123456",
->>>>>>> b60f829 (Lưu thay đổi của tôi trong src/index.js)
   database: process.env.DB_NAME || "phone_store",
   waitForConnections: true,
   connectionLimit: 20,
