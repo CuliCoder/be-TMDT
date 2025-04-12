2516998c805beb0f1140357d63a7dcfdf61597a7
trungnc113 <nguyencongtrung13@gmail.com>
2025-04-13 00:54:42 +0700

    api admin login

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,7 +6,13 @@ const hashPassword = (password) => {
   return bcrypt.hashSync(password, saltRounds);
 };
 
-export const registerUser = async (fullname, email, phonenumber, password, confirmPassword) => {
+export const registerUser = async (
+  fullname,
+  email,
+  phonenumber,
+  password,
+  confirmPassword
+) => {
   if (password !== confirmPassword) {
     throw { error: 1, message: "Mật khẩu nhập lại không khớp" };
   }
@@ -22,7 +28,10 @@ export const registerUser = async (fullname, email, phonenumber, password, confi
   }
 
   const checkQuery = `SELECT * FROM users WHERE Email = ? OR PhoneNumber = ?`;
-  const [existingUsers] = await database.execute(checkQuery, [email, phonenumber]);
+  const [existingUsers] = await database.execute(checkQuery, [
+    email,
+    phonenumber,
+  ]);
 
   if (existingUsers.length > 0) {
     throw { error: 1, message: "Email hoặc số điện thoại đã tồn tại" };
@@ -31,37 +40,69 @@ export const registerUser = async (fullname, email, phonenumber, password, confi
   const hashedPassword = hashPassword(password);
 
   const query = `INSERT INTO users (FullName, Email, PhoneNumber, PasswordHash, Role, status) VALUES (?, ?, ?, ?, ?, ?)`;
-  await database.execute(query, [fullname, email, phonenumber, hashedPassword, 'customer', 1]);
+  await database.execute(query, [
+    fullname,
+    email,
+    phonenumber,
+    hashedPassword,
+    "customer",
+    1,
+  ]);
 
   return { error: 0, message: "Đăng ký thành công" };
 };
 
-
 export const loginUser = async (identifier, password) => {
-  const query = `SELECT * FROM users WHERE (Email = ? OR PhoneNumber = ?) AND Status = 1`;
+  const query = `SELECT * FROM users WHERE (Email = ? OR PhoneNumber = ?) AND Status = 1 and Role = 'customer'`;
   const [users] = await database.execute(query, [identifier, identifier]);
 
   if (users.length === 0) {
-      throw { error: 1, message: "Tài khoản không tồn tại hoặc đã bị khóa" };
+    throw { error: 1, message: "Tài khoản không tồn tại hoặc đã bị khóa" };
   }
 
   const user = users[0];
   if (!bcrypt.compareSync(password, user.PasswordHash)) {
-      throw { error: 1, message: "Mật khẩu không chính xác" };
+    throw { error: 1, message: "Tài khoản hoặc mật khẩu không chính xác" };
   }
 
   return {
-      error: 0,
-      message: "Đăng nhập thành công",
-      data: {
-          id: user.UserID,
-          fullname: user.FullName,
-          email: user.Email,
-          phonenumber: user.PhoneNumber,
-          role: user.Role,
-      },
+    error: 0,
+    message: "Đăng nhập thành công",
+    data: {
+      id: user.UserID,
+      fullname: user.FullName,
+      email: user.Email,
+      phonenumber: user.PhoneNumber,
+      role: user.Role,
+    },
   };
 };
 
+export const loginAdmin = async (identifier, password) => {
+  try {
+    const query = `SELECT * FROM users WHERE (Email = ? OR PhoneNumber = ?) AND Status = 1 and Role = 'admin'`;
+    const [users] = await database.execute(query, [identifier, identifier]);
+    if (users.length === 0) {
+      return { error: 1, message: "Tài khoản không tồn tại hoặc đã bị khóa" };
+    }
 
-
+    const user = users[0];
+    if (!bcrypt.compareSync(password, user.PasswordHash)) {
+      return { error: 1, message: "Tài khoản hoặc mật khẩu không chính xác" };
+    }
+    return {
+      error: 0,
+      message: "Đăng nhập thành công",
+      data: {
+        id: user.UserID,
+        fullname: user.FullName,
+        email: user.Email,
+        phonenumber: user.PhoneNumber,
+        role: user.Role,
+      },
+    };
+  } catch (error) {
+    console.error("Lỗi trong loginAdmin:", error);
+    return { error: 1, message: "Đã xảy ra lỗi trong quá trình đăng nhập" };
+  }
+};
